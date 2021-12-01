4934796a992a0e7a68abca48824c35bcf9a00a1a
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2021-12-01 13:00:07 -0800

    commit tree c2ee92246bd505b332be0c47373151874ebe45e1

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance >= 2500) {
+    return "No can do.";
+  } else if (distance >= 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
+//function switchOnCharmFromTip(money) {
+  // Write your code here!
+//  let message;
+//  switch(money) {
+//    case 'generous':
+//      message = "Thank you so much.";
+//      break;
+//    case 'not as generous':
+//      message = "Thank you.";
+//      break;
+//    default:
+//      message = "Bye.";
+//      break;
+//  }
+//  return message
+//}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+        default:
+          return "Bye.";
+  }
 }
+// Refractored
+
+
+
